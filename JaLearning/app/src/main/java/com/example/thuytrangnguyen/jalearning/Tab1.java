652b34e3e71c29cc4de58c5e06a38c53069db262
trangnt08 <thuytrangnguyen1306@gmail.com>
2016-08-05 09:17:07 +0700

    main_layout

---
+++ JaLearning/app/src/main/java/com/example/thuytrangnguyen/jalearning/Tab1.java
@@ -0,0 +1,25 @@
+package com.example.thuytrangnguyen.jalearning;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thuy Trang Nguyen on 8/3/2016.
+ */
+public class Tab1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab1, container, false);
+    }
+}
